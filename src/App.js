8efb7b184f85cda708f408c5909f3cd85501abc3
Carlos Ortiz <101150271+CurthbertAllgood@users.noreply.github.com>
2024-01-22 13:25:04 -0300

    se actualiza el proyecto
    se corrigen errores y se suma un directorio nuevo

--- src/App.js
+++ src/App.js
@@ -7,45 +7,14 @@ app.use(bodyParser.json());
 const ProductManager = require("./ProductManager");
 const productManager = new ProductManager("products.json");
 
-const CartManager = require("./ProductManager"); // Puedes usar el mismo ProductManager para el carrito si lo prefieres
+const CartManager = require("./ProductManager");
 const cartManager = new CartManager("carts.json");
 
 // Rutas relacionadas con productos
-app.get('/products', async (req, res) => {
-    try {
-        const limit = parseInt(req.query.limit, 10);
-
-        if (!isNaN(limit) && limit >= 0) {
-            const products = await productManager.getProducts();
-            res.json({ products: products.slice(0, limit) });
-        } else {
-            res.status(400).json({ error: 'Invalid limit parameter' });
-        }
-    } catch (error) {
-        console.error(error);
-        res.status(500).json({ error: 'Internal Server Error' });
-    }
-});
-
-app.get('/products/:id', async (req, res) => {
-    const productId = parseInt(req.params.id, 10);
-
-    if (isNaN(productId) || productId <= 0) {
-        res.status(400).json({ error: 'Invalid product ID' });
-        return;
-    }
-
-    const product = await productManager.getProductById(productId);
-
-    if (product) {
-        res.json({ product });
-    } else {
-        res.json({ message: 'El ID del producto no fue encontrado' });
-    }
-});
+app.use('/api/products', require('./routes/api/products.router')); // Utiliza el enrutador de productos
 
 // Rutas relacionadas con el carrito
-app.use('/cart', require('./routes/carts.router'));
+app.use('/cart', require('./routes/api/carts.router'));
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
